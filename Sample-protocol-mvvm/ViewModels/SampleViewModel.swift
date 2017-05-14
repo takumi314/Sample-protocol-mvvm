dcd1a76184119c80d5cefdbf744be8d6a5829fb4
takumi314 <isospin_symmetry@me.com>
2017-05-15 02:42:18 +0900

    add an initializer

--- Sample-protocol-mvvm/ViewModels/SampleViewModel.swift
+++ Sample-protocol-mvvm/ViewModels/SampleViewModel.swift
@@ -10,18 +10,39 @@ import UIKit
 
 class SampleViewModel: CellRepresentable {
 
+    // MARK: - Internal propeties
+
+    var sampleModel: SampleModel
+
+    var price: String {
+        guard let price = sampleModel.price else {
+            return "No data"
+        }
+        return "\(price)"
+    }
+
     var rowHeight: CGFloat {
         get {
             return 70.0
         }
     }
 
+
+    // MARK: - Initializer
+
+    init(sampleModel: SampleModel) {
+        self.sampleModel = sampleModel
+    }
+
+
+    // MARK: - Internal methods
+
     func cellInstance(_ tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
         // Dequeue a cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! SampleTableViewCell
 
         // Pass ourselves (the view model) to setup the cell
-
+        cell.setup(viewModel: self)
 
         // Return the cell
         return cell
