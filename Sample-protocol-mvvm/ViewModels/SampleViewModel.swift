6e81c3566cb40f3d81a7a54fae61eea97a678bf1
takumi314 <isospin_symmetry@me.com>
2017-05-15 01:51:49 +0900

    confirm to CellRepresentable

--- Sample-protocol-mvvm/ViewModels/SampleViewModel.swift
+++ Sample-protocol-mvvm/ViewModels/SampleViewModel.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2017年 Kohey. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SampleViewModel: CellRepresentable {
+
+    var rowHeight: CGFloat {
+        get {
+            return 70.0
+        }
+    }
+
+    func cellInstance(_ tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+
+        // Pass ourselves (the view model) to setup the cell
+
+
+        // Return the cell
+        return cell
+    }
+
+}
