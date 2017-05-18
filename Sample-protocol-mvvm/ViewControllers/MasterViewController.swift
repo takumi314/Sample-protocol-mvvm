123748382414f627c1eed46b7a9f8e488201850a
takumi314 <isospin_symmetry@me.com>
2017-05-18 23:39:51 +0900

    make new ViewController

---
+++ Sample-protocol-mvvm/ViewControllers/MasterViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MasterViewController.swift
+//  Sample-protocol-mvvm
+//
+//  Created by NishiokaKohei on 2017/05/18.
+//  Copyright © 2017年 Kohey. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    // all of the data in the array conforms to the CellRepresentable protocol,
+    fileprivate var data = [CellRepresentable]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+
+extension MasterViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return data[indexPath.row].cellInstance(tableView, indexPath: indexPath)
+    }
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension MasterViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return data[indexPath.row].rowHeight
+    }
+}
