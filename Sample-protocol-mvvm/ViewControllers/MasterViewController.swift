a89fc5028eed31f1b2d503d06c5de24ae0ca36a4
takumi314 <isospin_symmetry@me.com>
2017-05-19 01:46:12 +0900

    implement an AlertViewController to register new task

--- Sample-protocol-mvvm/ViewControllers/MasterViewController.swift
+++ Sample-protocol-mvvm/ViewControllers/MasterViewController.swift
@@ -24,6 +24,8 @@ class MasterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         masterTitle.title = "Title"
+        masterTableView.delegate = self
+        masterTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,7 +35,39 @@ class MasterViewController: UIViewController {
     // MARK: - Actions
 
     @IBAction func didTapRightBarButton(_ sender: UIBarButtonItem) {
-        //
+        //textの表示はalertのみ。ActionSheetだとtextfiledを表示させようとすると
+        //落ちます。
+        let alert = UIAlertController(title:"action",
+                                      message: "alertView",
+                                      preferredStyle: UIAlertControllerStyle.alert)
+
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel) { (action: UIAlertAction) in print("Cancel") }
+        let defaultAction = UIAlertAction(title: "OK",
+                                          style: .default) { (action: UIAlertAction) in print("OK") }
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        
+        //textfiledの追加
+        alert.addTextField(configurationHandler: {(text: UITextField) in
+            //対象UITextFieldが引数として取得できる
+            text.placeholder = "Task name"
+        })
+        //実行した分textfiledを追加される。
+        alert.addTextField() { (content: UITextField) in
+            content.placeholder = "Content"
+        }
+        alert.addTextField() { (date: UITextField) in
+            date.placeholder = "Dur date"
+        }
+
+        let register = { [weak self] in
+
+            self?.masterTableView.reloadData()
+        }
+
+
+        present(alert, animated: true, completion: register as? () -> Void)
     }
 
 }
